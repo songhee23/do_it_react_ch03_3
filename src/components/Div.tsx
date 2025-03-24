fa1d7e884e9160468cc7691d7ed3ef9e047f8150
unknown <ereteyey@naver.com>
2025-03-24 17:05:43 +0900

    package.json 수정

---
+++ src/components/Div.tsx
@@ -0,0 +1,17 @@
+import type {FC, DetailedHTMLProps, HTMLAttributes, PropsWithChildren} from 'react'
+import type {WidthHeight} from './WidthHeight'
+
+export type ReactDivProps = DetailedHTMLProps<
+    HTMLAttributes<HTMLDivElement>, 
+    HTMLDivElement
+>
+
+export type DivProps = ReactDivProps & PropsWithChildren<WidthHeight>
+
+// prettier-ignore
+export const Div: FC<DivProps> = ({
+    width, height, style: _style, ...props
+}) => {
+    const style = {..._style, width, height}
+    return <div {...props} style={style} />
+}
